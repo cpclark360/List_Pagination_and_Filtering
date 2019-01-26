c0681ac888fde1831b973fc1837a429e89c95d24
Cedric Clark <cpclark360@gmail.com>
2019-01-25 21:55:19 -0600

    update
    updated

--- js/script.js
+++ js/script.js
@@ -1,42 +1,18 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+Cedric Clark
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 
 const unorderedList = document.getElementsByClassName('student-list')[0];
 const studentList = unorderedList.children;
 const pageLinks = document.getElementsByClassName('page')[0];
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
+// 
 
 const showPage = (list, pageNumber) => {
 
@@ -50,10 +26,7 @@ const showPage = (list, pageNumber) => {
 }
 
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+//
 
 const appendPageLinks = () => {
 	const studentNum = studentList.length;
@@ -80,19 +53,37 @@ const appendPageLinks = () => {
 		newAnchor.href = '#';
 		newAnchor.textContent = i+1;
 		newLi.appendChild(newAnchor);
-		newAnchor.addEventListener("click", () => {
+		
+		// Cant figure out why the 6th button className isnt being changed after its been clicked.
+		newAnchor.addEventListener("click", (e) => {
+			const aList = document.getElementsByTagName('a');
+			for( let i = 0; i < aList.length ; i += 1){
+				if ( aList[i].className === '' ){
+					e.target.className = 'active';
+					console.log(i);
+				} else {
+					aList[i].className = '';
+				}
+			}
 			showPage(studentList, i);
-
  		});
 	}
 }
 
-const aList = document.getElementsByTagName('a');
+
+
+
+
 
 
 showPage(studentList,0);
 appendPageLinks();
 
+const aList = document.getElementsByTagName('a');
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+
+
+
+
+// Remember to delete the comments that came with this file, and replace them with your own code comments
