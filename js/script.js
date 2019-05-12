71d6c6c300663c6c47e7d6fb6be50b57a4966c2b
cpclark360 <40728556+cpclark360@users.noreply.github.com>
2019-05-12 02:35:41 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,19 +4,13 @@ FSJS project 2 - List Filter and Pagination
 Cedric Clark
 ******************************************/
    
-
-
 // Defined Global varibles
-
 const unorderedList = document.getElementsByClassName('student-list')[0];
 const studentList = unorderedList.children;
 const pageLinks = document.getElementsByClassName('page')[0];
 
-
 //  'showPage' function displays 10 items per page until its fewer than 10 and hides the remaining list items.
-
 const showPage = (list, pageNumber) => {
-
   for(let i = 0; i < list.length; i += 1){
       if ( i < pageNumber * 10 || i >= ( pageNumber + 1 ) * 10 ){
         list[i].style.display = 'none';
@@ -25,11 +19,8 @@ const showPage = (list, pageNumber) => {
       }
   }
 }
-
-
 // 'appendPageLinks' function dynamically creates and loads the HTML
 //  eventListener on anchor tags 0-5  listens for the anchor clicked and changes the 'className' value to 'active' */
-
 const appendPageLinks = () => {
 	const studentNum = studentList.length;
 	const pageAmount = Math.ceil( studentNum / 10 );
@@ -45,7 +36,6 @@ const appendPageLinks = () => {
 	
 
 	// pageAmount + 1 is to end event target on the 7th anchor
-
 	for(let i = 0; i < pageAmount + 1; i += 1){ 
   		const newLi = document.createElement('li');
 		const newAnchor = document.createElement('a');
@@ -55,21 +45,17 @@ const appendPageLinks = () => {
 		newAnchor.href = '#';
 		newAnchor.textContent = i+1; // to change anchor text from 0-5 to 1-7
 		newLi.appendChild(newAnchor);
-		
-		
-
+	
 		newAnchor.addEventListener("click", (e) => {
 			const aList = document.getElementsByTagName('a');
 			for( let i = 0; i < aList.length ; i += 1){
 				if ( aList[i].className === '' ){
-					e.target.className = 'active';
-					
+					e.target.className = 'active';				
 				} else {
 					aList[i].className = '';
 				}
 			}
-			showPage(studentList, i);
-			
+			showPage(studentList, i);		
  		});
 	}
 
@@ -82,7 +68,6 @@ const appendPageLinks = () => {
 }
 
 // 'showPage' starts on the first page
-
 showPage(studentList,0);
 appendPageLinks();
 
