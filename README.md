ddee829636ed662f9e5f42396dcda9f1abbd0cea
cpclark360 <40728556+cpclark360@users.noreply.github.com>
2019-06-29 15:09:57 -0500

    added Markdown

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# List_Pagination_and_Filtering
+# List Pagination
+
+> Receives a List of user profiles and displays 10 and hides the remaining list, in addition to adding a page number that lists the following 10 until all remaining profiles have a page number.
+
+# Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+# Directory Tree
+
+```
+   ├─ README.md
+   ├─ css
+   │  ├─ design.css
+   │  └─ reset.css
+   ├─ examples
+   │  ├─ 44students.html
+   │  ├─ 64students.html
+   │  ├─ example-exceeds.html
+   │  └─ example-meets.html
+   ├─ index.html
+   └─ js
+      └─ script.js
+```
+
+# Usage
+
+> Using a web browser preferably Google Chrome or FireFox.
--- js/script.js
+++ js/script.js
@@ -3,87 +3,76 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 Cedric Clark
 ******************************************/
-   
-
 
 // Defined Global varibles
 
-const unorderedList = document.getElementsByClassName('student-list')[0];
+const unorderedList = document.getElementsByClassName("student-list")[0];
 const studentList = unorderedList.children;
-const pageLinks = document.getElementsByClassName('page')[0];
-
+const pageLinks = document.getElementsByClassName("page")[0];
 
 //  'showPage' function displays 10 items per page until its fewer than 10 and hides the remaining list items.
 
 const showPage = (list, pageNumber) => {
-
-  for(let i = 0; i < list.length; i += 1){
-      if ( i < pageNumber * 10 || i >= ( pageNumber + 1 ) * 10 ){
-        list[i].style.display = 'none';
-      } else {
-        list[i].style.display = 'block';
-      }
+  for (let i = 0; i < list.length; i += 1) {
+    if (i < pageNumber * 10 || i >= (pageNumber + 1) * 10) {
+      list[i].style.display = "none";
+    } else {
+      list[i].style.display = "block";
+    }
   }
-}
-
+};
 
 // 'appendPageLinks' function dynamically creates and loads the HTML
 //  eventListener on anchor tags 0-5  listens for the anchor clicked and changes the 'className' value to 'active' */
 
 const appendPageLinks = () => {
-	const studentNum = studentList.length;
-	const pageAmount = Math.ceil( studentNum / 10 );
-	
-	const newDiv = document.createElement("div");
-	newDiv.className = 'pagination';
-	newDiv.textContent = '';
-	pageLinks.appendChild(newDiv);
-	
-	const newUL = document.createElement('ul');
-	const activeSelection = document.getElementsByClassName('.pagination').firstChild;
-	newDiv.appendChild(newUL);
-	
+  const studentNum = studentList.length;
+  const pageAmount = Math.ceil(studentNum / 10);
 
-	// pageAmount + 1 is to end event target on the 7th anchor
+  const newDiv = document.createElement("div");
+  newDiv.className = "pagination";
+  newDiv.textContent = "";
+  pageLinks.appendChild(newDiv);
 
-	for(let i = 0; i < pageAmount + 1; i += 1){ 
-  		const newLi = document.createElement('li');
-		const newAnchor = document.createElement('a');
-		newUL.appendChild(newLi);
-		
-		newAnchor.className = '';
-		newAnchor.href = '#';
-		newAnchor.textContent = i+1; // to change anchor text from 0-5 to 1-7
-		newLi.appendChild(newAnchor);
-		
-		
+  const newUL = document.createElement("ul");
+  const activeSelection = document.getElementsByClassName(".pagination")
+    .firstChild;
+  newDiv.appendChild(newUL);
 
-		newAnchor.addEventListener("click", (e) => {
-			const aList = document.getElementsByTagName('a');
-			for( let i = 0; i < aList.length ; i += 1){
-				if ( aList[i].className === '' ){
-					e.target.className = 'active';
-					
-				} else {
-					aList[i].className = '';
-				}
-			}
-			showPage(studentList, i);
-			
- 		});
-	}
+  // pageAmount + 1 is to end event target on the 7th anchor
 
-	// starts with first anchor  'className' value as 'active'
-	// Hides the last child anchor tag
+  for (let i = 0; i < pageAmount + 1; i += 1) {
+    const newLi = document.createElement("li");
+    const newAnchor = document.createElement("a");
+    newUL.appendChild(newLi);
 
-	const aList = document.getElementsByTagName('a');
-	aList[0].className = 'active';
-	newUL.lastChild.style.display = 'none';
-}
+    newAnchor.className = "";
+    newAnchor.href = "#";
+    newAnchor.textContent = i + 1; // to change anchor text from 0-5 to 1-7
+    newLi.appendChild(newAnchor);
+
+    newAnchor.addEventListener("click", e => {
+      const aList = document.getElementsByTagName("a");
+      for (let i = 0; i < aList.length; i += 1) {
+        if (aList[i].className === "") {
+          e.target.className = "active";
+        } else {
+          aList[i].className = "";
+        }
+      }
+      showPage(studentList, i);
+    });
+  }
+
+  // starts with first anchor  'className' value as 'active'
+  // Hides the last child anchor tag
+
+  const aList = document.getElementsByTagName("a");
+  aList[0].className = "active";
+  newUL.lastChild.style.display = "none";
+};
 
 // 'showPage' starts on the first page
 
-showPage(studentList,0);
+showPage(studentList, 0);
 appendPageLinks();
-
-
